cff9f946386cfba1b53cce15b876b25c4eeeca64
Francis Tayag <zyenapz@gmail.com>
2025-02-05 13:58:17 +0800

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch(choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
