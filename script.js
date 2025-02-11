a09c950ad38d174356ae6203fd155318668745c5
Francis Tayag <zyenapz@gmail.com>
2025-02-11 10:59:47 +0800

    Improve QoL and add a way to reset the game

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 let humanScore = 0;
 let computerScore = 0;
+let isGameOver = false;
 
 let buttonRock = document.querySelector("button#rock");
 let buttonPaper = document.querySelector("button#paper");
 let buttonScissor = document.querySelector("button#scissor");
+let buttonReset = document.querySelector("button#reset");
 let paraResults = document.querySelector("p#results");
 let dispScoreHuman = document.querySelector("span#player"); 
 let dispScoreComputer = document.querySelector("span#computer"); 
 let options = document.querySelector("div#options");
+
+buttonReset.style.visibility = "hidden";
 
 options.addEventListener("click", (e) => {
     const humanChoice = e.target.id;
     playGame(humanChoice); // Entry code
 });
 
-function playGame(humanChoice) {
-    const computerChoice = getComputerChoice();
+buttonReset.addEventListener("click", () => {
+    resetGame();
+});
 
-    let winner = playRound(humanChoice, computerChoice);
-
-    switch(winner) {
-        case "human": 
-            humanScore++; 
-            break;
-        case "computer": 
-            computerScore++; 
-            break;
-    }
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    isGameOver = false;
 
     dispScoreHuman.textContent = humanScore;
     dispScoreComputer.textContent = computerScore;
+    paraResults.textContent = "Click any button!";
+    buttonReset.style.visibility = "hidden";
+}
 
-    if (humanScore >= 5 || computerScore >= 5) {
-        displayGameWinner(humanScore, computerScore);
+function playGame(humanChoice) {
+    if(!isGameOver) {
+        const computerChoice = getComputerChoice();
+
+        let winner = playRound(humanChoice, computerChoice);
+
+        switch(winner) {
+            case "human": 
+                humanScore++; 
+                break;
+            case "computer": 
+                computerScore++; 
+                break;
+        }
+
+        dispScoreHuman.textContent = humanScore;
+        dispScoreComputer.textContent = computerScore;
+
+        if (humanScore >= 5 || computerScore >= 5) {
+            displayGameWinner(humanScore, computerScore);
+            buttonReset.style.visibility = "visible";
+            isGameOver = true;
+        }
     }
 }
 
